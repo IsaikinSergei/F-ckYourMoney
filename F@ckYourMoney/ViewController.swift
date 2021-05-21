5e7f8845f1f8e45e3bc5e150e823634f5aeb9421
IsaikinSergei <akinfest007@gmail.com>
2021-05-21 23:42:29 +0400

    Added logics for buttons "numberFromKeyboard"

--- F@ckYourMoney/ViewController.swift
+++ F@ckYourMoney/ViewController.swift
@@ -9,11 +9,37 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var displaylabel: UILabel!
+    var stillTyping = false
+    
+    @IBOutlet var numberFromKeyboard: [UIButton]! {
+        didSet {
+            for button in numberFromKeyboard {
+                button.layer.cornerRadius = 11
+            }
+        }
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
     }
 
-
+    // MARK: - добавляем логику по нажатию на кнопки клавиатуры
+    @IBAction func numberPressed(_ sender: UIButton) {
+        let number = sender.currentTitle!
+        
+        // делаем проверку в displaylabel
+        if stillTyping {
+            if displaylabel.text!.count  < 10 {
+                displaylabel.text = displaylabel.text! + number
+            }
+        } else {
+            displaylabel.text = number
+            stillTyping = true
+        }
+        
+    }
+    
 }
 
